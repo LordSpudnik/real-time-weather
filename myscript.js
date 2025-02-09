0bcdb696a7d0db9d5206afa1244b7ca3b1ec749e
Subash Venkat <subash.venkat2023@vitstudent.ac.in>
2025-02-09 19:54:12 +0530

    added a logo to the title bar of the webpage

--- myscript.js
+++ myscript.js
@@ -26,6 +26,7 @@ async function checkWeather(city) {
         document.querySelector('.weather-icon').src = "";
         document.querySelector('.city').innerHTML = "";
         document.querySelector('.temp').innerHTML = "";
+        document.body.style.backgroundImage = "url('./Backgrounds/FirstBg.png')";
         document.querySelector('.weather-container').style.background = "rgb(255, 255, 255, 0.2)";
         return;
     }
