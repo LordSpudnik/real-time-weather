56dd081cd49ce4c80983cd03d3f2b33224210aec
Subash Venkat <subash.venkat2023@vitstudent.ac.in>
2025-02-09 18:59:25 +0530

    Added dynamic background and made some css changes

--- myscript.js
+++ myscript.js
@@ -15,15 +15,18 @@ async function checkWeather(city) {
         document.querySelector('.weather-icon').src = "";
         document.querySelector('.city').innerHTML = "";
         document.querySelector('.temp').innerHTML = "";
+        document.body.style.backgroundImage = "url('./Backgrounds/FirstBg.png')";
+        document.querySelector('.weather-container').style.background = "rgb(255, 255, 255, 0.2)";
         return;
     }
 
-    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    const response = await fetch(`${apiUrl}${city}&appid=${apiKey}`);
     if(!response.ok) {
         document.querySelector('.error-msg').style.display = "block";
         document.querySelector('.weather-icon').src = "";
         document.querySelector('.city').innerHTML = "";
         document.querySelector('.temp').innerHTML = "";
+        document.querySelector('.weather-container').style.background = "rgb(255, 255, 255, 0.2)";
         return;
     }
 
@@ -33,20 +36,28 @@ async function checkWeather(city) {
     console.log(data);
     document.querySelector('.city').innerHTML = data.name;
     document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector('.weather-container').style.backgroundColor = "rgba(54, 47, 47, 0.8)";
 
     if(data.weather[0].main === "Clear") {
         document.querySelector('.weather-icon').src = "./Icons/Sunny.png";
+        document.body.style.backgroundImage = "url('./Backgrounds/Sunny.jpg')";
     } else if(data.weather[0].main === "Clouds") {
         document.querySelector('.weather-icon').src = "./Icons/Cloudy.png";
+        document.body.style.backgroundImage = "url('./Backgrounds/cloudy.jpg')";
     } else if(data.weather[0].main === "Rain") {
         document.querySelector('.weather-icon').src = "./Icons/Rains.png";
+        document.body.style.backgroundImage = "url('./Backgrounds/rainy.jpg')";
     } else if(data.weather[0].main === "Snow") {
         document.querySelector('.weather-icon').src = "./Icons/Snowy.png";
+        document.body.style.backgroundImage = "url('./Backgrounds/snowy.jpg')";
     } else if(data.weather[0].main === "Mist") {
         document.querySelector('.weather-icon').src = "./Icons/Mist.png";
+        document.body.style.backgroundImage = "url('./Backgrounds/Misty.jpg')";
     } else if(data.weather[0].main === "Drizzle") {
         document.querySelector('.weather-icon').src = "./Icons/Drizzle.png";
+        document.body.style.backgroundImage = "url('./Backgrounds/Drizzle.jpg')";
     } else if(data.weather[0].main === "Haze") {
         document.querySelector('.weather-icon').src = "./Icons/Haze.png";
+        document.body.style.backgroundImage = "url('./Backgrounds/Haze.jpg')";
     }
 }
