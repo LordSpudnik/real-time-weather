d72a7b516d749e50886548571753a58467c1050a
Subash Venkat <subash.venkat2023@vitstudent.ac.in>
2025-02-12 13:09:34 +0530

    added city for alert

--- myscript.js
+++ myscript.js
@@ -90,11 +90,12 @@ function submitPhoneNumber(){
 
 document.getElementById('alertForm').addEventListener('submit', function(event) {
     event.preventDefault();
+    const city = document.querySelector('.search-bar input').value;
     const tempThreshold = document.getElementById('tempThreshold').value;
     const weatherType = document.getElementById('weatherType').value;
     const alertTime = document.getElementById('alertTime').value;
     
-    alert(`Alert set for temperature above ${tempThreshold}°C and weather type ${weatherType} within the next ${alertTime} hours.`);
+    alert(`Alert set for temperature above ${tempThreshold}°C and weather type ${weatherType} within the next ${alertTime} hours in ${city}.`);
     document.querySelector('.form-container').classList.remove('active');
     document.querySelector('.weather-container').style.transform = 'translateX(0)';
     document.getElementById('tempThreshold').value = "";
